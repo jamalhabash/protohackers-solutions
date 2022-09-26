8a5b77abf51c6722b7e992fe743caa63e9e93aac
jhabash <jamal.m.habash@gmail.com>
2022-09-26 14:51:57 -0400

    Modified to use read_until() function, which does not enforce valid utf8, all tests now passing

--- smoke_test/src/lib.rs
+++ smoke_test/src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::SocketAddr;
 use std::net::{Shutdown, TcpListener, TcpStream};
 
@@ -41,31 +41,37 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 fn handle_connection(stream: TcpStream) {
     let mut stream = BufReader::new(stream);
     //maybe you can use a while let here instead of a loop
+    println!("We have a new connection!");
+
     loop {
         let mut buf = String::new();
+        let mut buf = Vec::new();
         //read line will return ok(0) if it is end of file so use a match statement and end.break
         //stream.get_ref().shutdown(how) probably use this to close the connection on EOF
-        match stream.read_line(&mut buf) {
+        match stream.read_until(0xA, &mut buf) {
             Ok(0) => {
+                println!("EOL reached");
+
                 stream
                     .get_ref()
                     .shutdown(Shutdown::Both)
                     .expect("shutdown call failed");
                 break;
             }
-            Err(_) => {
-                println!("test1");
+            Err(e) => {
+                println!("There was an error {}", e);
                 break;
             }
             _ => (),
         }
-
-        println!("test");
         // if stream.read_line(&mut buf).is_err() {
         //     break; //check to see if connection is every terminated
         // }
-        stream.get_ref().write(buf.as_bytes()).unwrap(); //TODO
+        println!("{:?}", buf);
+        stream.get_ref().write(&buf).unwrap(); //TODO
     }
+
+    println!("We are out of loop!")
 }
 
 // #[cfg(test)]
